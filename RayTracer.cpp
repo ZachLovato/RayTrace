5c6bf30b6b8f1bee82d43ea9594010d0cb3294bc
ZachLovato <zlovato@student.neumont.edu>
2022-12-08 15:34:15 -0700

    Plane

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,24 +1,28 @@
 #include "Renderer/renderer.h"
+#include "Renderer/Camera.h"
 #include "Objects/Sphere.h"
 #include "Objects/Scene.h"
+#include "Objects/Plane.h"
 #include <iostream>
 
 int main(int, char**)
 {
+    const int width = 600;
+    const int height = 300;
+    const int samples = 10;
+
     Renderer rend;
     rend.Initialize();
-    rend.CreateWindow(600, 300);
+    rend.CreateWindow(width, height);
 
-    Canvas canvas(600, 300, rend);
-    Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 85.0f, 600 / (float)300);
+    Canvas canvas(width, height, rend);
+    Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 85.0f, width / (float)height);
     Scene scene;
 
-    //scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 100.0f, std::make_unique<Lambertian>(color3( 0.2f, 0.2f, 0.2f))));
-    //scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3( 0.2f, 0.2f, 0.2f))));
-
-    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ .7f, .5f, .4f })));
-    //scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 14.0f, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ .1f, .5f, .4f })));
-    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0.4f, 0.2f, 1 })));
+    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ .4f, .4f, .9f })));
+    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ -1.3, 0, -1 }, 0.8f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.3f)));
+    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ .1f, .7f, .2f })));
+    scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, 0, -8 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
 
     bool quit = false;
     while (!quit)
@@ -43,15 +47,7 @@ int main(int, char**)
         //      -- renderer Scene --
         canvas.Clear({ 0, 0, 0, 1 });
 
-        //      --- Dots ---
-        //for (int i = 0; i <= 10000; i++)
-        //{
-        //    //canvas.DrawPoint({ random(0, 400), random(0, 300)}, {random01(), random01(), random01(), 1});
-
-        //}
-
-
-        rend.Render(canvas, scene, camera, 100);
+        rend.Render(canvas, scene, camera, samples);
         canvas.Update();
 
         rend.CopyCanvas(canvas);
